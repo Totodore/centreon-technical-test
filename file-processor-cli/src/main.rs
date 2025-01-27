6616684e724bdc80a50f18b4e2734b2d1e22e857
Théodore Prévot <prevottheodore@gmail.com>
2025-01-27 15:36:19 +0100

    tests && doc

--- file-processor-cli/src/main.rs
+++ file-processor-cli/src/main.rs
@@ -1,4 +1,6 @@
 use file_processor::FileProcessorStreamExt;
+use futures_util::{stream, StreamExt};
+use tokio::{fs::File, io::BufReader};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,11 +10,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .init();
 
     let files = std::env::args().skip(1).collect::<Vec<_>>();
-    let result = files
-        .iter()
-        .map(String::as_str)
-        .count_line_words_concurrent()
-        .await;
+    let data_stream = stream::iter(files.iter()).filter_map(open_file);
+    let result = data_stream.count_line_words_concurrent().await;
     serde_json::to_writer_pretty(std::io::stdout(), &result)?;
     Ok(())
+}
+
+async fn open_file(path: &String) -> Option<(&str, BufReader<File>)> {
+    let file = File::open(&path)
+        .await
+        .inspect_err(|e| log::warn!("Could not open {path}, {e}, skipping it."))
+        .ok()?;
+    Some((path, BufReader::new(file)))
 }
--- file-processor/src/lib.rs
+++ file-processor/src/lib.rs
@@ -1,29 +1,36 @@
 use std::{collections::HashMap, future::Future};
 
 use futures_util::{Stream, StreamExt};
-use tokio::{
-    fs::File,
-    io::{AsyncBufRead, AsyncBufReadExt, BufReader},
-};
+use tokio::io::{AsyncBufRead, AsyncBufReadExt};
 use tokio_stream::wrappers::LinesStream;
 
-/// Extension trait for iterators over file paths.
-/// This is a convenience method to count the number of words in each line of each file concurrently.
+/// Extension trait for stream over async readers bound to a string identifier.
+///
+/// This is a convenience method to count the number of words in each line of each data stream concurrently.
 /// For more control, use the `count_line_words_concurrent` function directly.
-pub trait FileProcessorStreamExt<'a>: Iterator<Item = &'a str> + Sized {
-    /// Open files concurrently and count the number of words in each line.
-    /// Returns a map of paths to a vector of word counts for each line.
+pub trait FileProcessorStreamExt<'a, R>: Stream<Item = (&'a str, R)> + Sized
+where
+    R: AsyncBufRead + Unpin,
+{
+    /// Count the number of words from a stream of async readers and associated identifiers.
+    /// Returns a map of the identifier to a vector of word counts for each line.
     ///
-    /// If a file cannot be opened, it will be skipped with a warning log.
+    /// The readers will be polled concurrently.
     fn count_line_words_concurrent(self) -> impl Future<Output = HashMap<&'a str, Vec<usize>>> {
-        let files = futures_util::stream::iter(self).filter_map(open_file);
-        count_line_words_concurrent(files)
+        count_line_words_concurrent(self)
     }
 }
-impl<'a, I: Iterator<Item = &'a str>> FileProcessorStreamExt<'a> for I {}
+impl<'a, R, S> FileProcessorStreamExt<'a, R> for S
+where
+    R: AsyncBufRead + Unpin,
+    S: Stream<Item = (&'a str, R)>,
+{
+}
 
-/// Count the number of words from a stream of file readers and associated paths.
+/// Count the number of words from a stream of async readers and associated paths.
 /// Returns a map of paths to a vector of word counts for each line.
+///
+/// The files will be read concurrently.
 pub async fn count_line_words_concurrent<'a, R: AsyncBufRead + Unpin>(
     rds: impl Stream<Item = (&'a str, R)>,
 ) -> HashMap<&'a str, Vec<usize>> {
@@ -39,25 +46,33 @@ pub async fn count_line_words_concurrent<'a, R: AsyncBufRead + Unpin>(
 }
 
 /// Returns a stream of the number of words for each line of the input.
-pub fn count_line_words<R: AsyncBufRead>(
-    (path, rd): (&str, R),
-) -> impl Stream<Item = (&str, usize)> {
+/// The input path will be included in the output.
+fn count_line_words<R: AsyncBufRead>((path, rd): (&str, R)) -> impl Stream<Item = (&str, usize)> {
     LinesStream::new(rd.lines())
         .map(|line| line.unwrap().split_whitespace().count())
         .map(move |itm| (path, itm))
 }
 
-async fn open_file(path: &str) -> Option<(&str, BufReader<File>)> {
-    let file = File::open(&path)
-        .await
-        .inspect_err(|e| log::warn!("Could not open {path}, {e}, skipping it."))
-        .ok()?;
-    Some((path, BufReader::new(file)))
-}
-
 #[cfg(test)]
 mod tests {
+    use std::io;
+
+    use futures_util::stream;
+    use tokio::io::BufReader;
+
     use super::*;
+
+    #[tokio::test]
+    async fn test_count_line_words_concurrent() {
+        const FILE1: &str = include_str!("../../../tests/file1.txt");
+        const FILE2: &str = include_str!("../../../tests/file2.txt");
+        let iter = [("file1.txt", FILE1), ("file2.txt", FILE2)];
+        let stream =
+            stream::iter(iter.map(|(path, buff)| (path, BufReader::new(io::Cursor::new(buff)))));
+        let result = count_line_words_concurrent(stream).await;
+        assert_eq!(result.get("file1.txt").unwrap(), &[2, 3]);
+        assert_eq!(result.get("file2.txt").unwrap(), &[2, 2]);
+    }
 
     #[tokio::test]
     async fn test_count_line_words() {
@@ -70,7 +85,7 @@ mod tests {
             nulla pariatur. Excepteur sint occaecat cupidatat non proident\n,
             sunt in culpa qui officia deserunt mollit anim id est laborum.""#,
         );
-        let rd = BufReader::new(std::io::Cursor::new(DATA.1));
+        let rd = BufReader::new(io::Cursor::new(DATA.1));
         let counts: Vec<usize> = count_line_words((DATA.0, rd))
             .map(|(_, count)| count)
             .collect()
